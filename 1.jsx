975cb648fec192f85524256f1b8e3ea9bdafa3e7
Colin Fendrick <c-colin.fendrick@humanaedge.com>
2020-09-02 12:16:10 -0400

    :sparkles: Chp1

---
+++ 1.jsx
@@ -0,0 +1,11 @@
+const compose = (f, g) => x => f(g(x));
+
+const double = n => n*2;
+const inc = n => n + 1;
+const transform = compose(double, inc);
+transform(3); // 8
+
+/* x is 3 and passed into g, or inc, first
+  Then inc(3) is passed to f, or double,
+  8 is returned
+*/
