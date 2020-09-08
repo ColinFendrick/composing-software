22395cc2daaae73639aaae7f7809b8e993736203
Colin Fendrick <c-colin.fendrick@humanaedge.com>
2020-09-08 07:51:45 -0400

    :bug: bugfix

--- purefunctions.js
+++ purefunctions.js
@@ -1,8 +1,8 @@
 // Destructuring reducer example
 const myReducer = (state = {}, action = {}) => {
-  const { type, payload } = action;
-  switch (type) {
-    case 'FOO': return Object.assign({}, state, payload);
-    default: return ({...state});
-  }
+	const { type, payload } = action;
+	switch (type) {
+	case 'FOO': return Object.assign({}, state, payload);
+	default: return state;
+	}
 };
