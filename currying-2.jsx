5f09f9db6519aece1cdf249b70362ca230486824
Colin Fendrick <c-colin.fendrick@humanaedge.com>
2020-09-08 06:35:30 -0400

    :sparkles: More currying stuff

---
+++ currying-2.jsx
@@ -0,0 +1,29 @@
+/* Curried functions take multiple args one at a time
+  Here add() takes one arg, returns a partial application of itself
+  with 'a' fixed in closure scope
+*/
+const add = a => b => a + b;
+const five = add(2)(3);
+
+/* Partial application is a function which has been applied to some but not all of its args
+  and take as many or as few args as desired. Curried fns always return unary funcs, ooth,
+  and curried fns always return partial applications.
+  However, partial applications can be created outside of curried fns
+*/
+
+// Point-free style creates fn definitions which do not reference fn arguments
+
+const inc = add(1); // the partial application of add
+const four = inc(3);
+
+// Why curry? To simplify and modularize our code:
+const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
+const f = n => n * 2;
+const g = n => n + 1;
+const h = n => n - 4;
+
+const fortyTwo = compose(f, g, h)(24);
+// 24 - 4 + 1 * 2
+let thirty = compose(g, g, h, f)(16);
+thirty = compose(h, g, g, f, f)(8); // also
+const eight = compose()(8);
