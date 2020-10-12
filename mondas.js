3a096e66b95a8f2540f52ab63b49af9b5da3927f
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-12 13:31:20 -0400

    :sparkles: mondas

---
+++ mondas.js
@@ -0,0 +1,12 @@
+import { f as double } from './functions.js';
+
+const x = 20;
+const arr = [x]; // type lift
+const res = arr.map(double); 
+
+const echo = n => x =>
+  Array.from({ length: n }).fill(x);
+
+console.log([1,2,3].map(echo(3)));
+
+console.log([1,2,3].flatMap(echo(3)));
