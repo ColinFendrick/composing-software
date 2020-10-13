0a1ba7a681443502fc58d10cc2cc30c90ca3238e
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-13 11:43:08 -0400

    :wrench: Add eslint to devdeps cause I am dumb

---
+++ mondas2.js
@@ -0,0 +1,12 @@
+import { compose, trace } from './functions.js';
+
+const label = 'API call composition';
+const getUserById = id => id === 3 ? Promise.resolve({
+	name: 'Kurt',
+	role: 'Author'
+}) : undefined;
+
+const hasPermission = ({ role }) =>
+	Promise.resolve(role === 'Author');
+
+const authUser = compose(hasPermission, getUserById);
