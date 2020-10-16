9e6ed49161de60e10034cdf4065ca46c4cab96e3
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-16 16:17:01 -0400

    concatenation

--- object-composition.js
+++ object-composition.js
@@ -1,11 +1,5 @@
 import { assign } from './functions.js';
 
-const objs = [
-	{ a:'a',b:'ab' },
-	{ b:'b' },
-	{ c:'c',b:'cb' }
-];
-
 const sum = (a, b) => a + b;
 const total = [1, 2, 3].reduce(sum, 0);
 console.log(total);
