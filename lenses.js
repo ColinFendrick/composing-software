f4a815c58c6bf8dd3f66cb2b3d51f8f8f04bb170
Colin Fendrick <colin.fendrick@gmail.com>
2020-10-26 08:46:12 -0400

    Lenses pt2: proving lens laws

--- lenses.js
+++ lenses.js
@@ -1,7 +1,9 @@
+import isEqual from 'lodash/isEqual.js';
+
 const view = (lens, store) => lens.view(store);
 const set = (lens, value, store) => lens.set(value, store);
 
-const lensProp = prop => ({
+const createLens = prop => ({
 	view: store => store[prop],
 	set: (value, store) => ({
 		...store,
@@ -13,8 +15,40 @@ const fooStore = {
 	a: 'foo', b: 'bar'
 };
 
-const [aLens, bLens] = [lensProp('a'), lensProp('b')];
+const [aLens, bLens] = [createLens('a'), createLens('b')];
 
 const [a, b] = [view(aLens, fooStore), view(bLens, fooStore)];
 
 console.log(a, b);
+
+const bazStore = set(aLens, 'baz', fooStore);
+console.log(view(aLens, bazStore));
+
+// Proving laws:
+const store = fooStore;
+
+// `view(lens, set(lens, value, store))` === `value`
+// If you set a value into the store, and immediately view the value
+// through the lens, you get the value that was set.
+{
+	const lens = createLens('a');
+	const value = 'baz';
+	const a = value;
+	const b = view(lens, set(lens, value, store));
+
+	console.log(a === b);
+}
+
+{
+	// set(lens, b, set(lens, a, store)) = set(lens, b, store)
+	// If you set a lens value to `a` and then immediately set the lens value to `b`,
+	// it's the same as if you'd just set the value to `b`.
+	const lens = createLens('a');
+
+	const a = 'bar';
+	const b = 'baz';
+
+	const r1 = set(lens, b, set(lens, a, store));
+	const r2 = set(lens, b, store);
+	console.log(isEqual(r1, r2));
+}
