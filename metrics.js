29f6bf3dcd8ddd593a6dbf5315f5ba27330cda6b
Colin Fendrick <colin.fendrick@gmail.com>
2021-02-06 09:17:06 -0800

    Cartesian and lpspaces

--- metrics.js
+++ metrics.js
@@ -1,24 +1,27 @@
-const apply = ({ metric, range }) => (x1, x2) => range(x1) && range(x2) && metric(x1, x2);
+const apply = ({ range, metric }) => (x1, x2) => range(x1) && range(x2) && metric(x1, x2);
 const metricSpace = (range, metric) => ({ range, metric });
 
-const realNumbers = x => typeof x === 'number';
-const integers = x => Number.isInteger(x);
+const realNumbers = n => typeof n === 'number';
+const integers = n => Number.isInteger(n);
+const coordinates = c => c.every(n => integers(n));
+
 const realMetric = (x1, x2) => Math.abs(x2 - x1);
+const cartesianMetric = ([x1, y1], [x2, y2]) => Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 const bunchedIntegerMetric = (x1, x2) => x1 % 2 !== 0 || x2 % 2 !== 0 ?
 	(Math.abs(x2 - x1) / 2) + 1 :
 	(Math.abs(x2 - x1) / 2);
+const taxicabMetric = ([x1, y1], [x2, y2]) => Math.abs(x2 - x1) + Math.abs(y2 - y1);
 
 const real = metricSpace(realNumbers, realMetric);
-
 const natural = metricSpace(integers, realMetric);
-
 const evenCountingNatural = metricSpace(integers, bunchedIntegerMetric);
-
-// const taxiCabMetric =
-
+const cartesian = metricSpace(coordinates, cartesianMetric);
+const lpSpace = metricSpace(coordinates, taxicabMetric);
 
 console.log(
 	// apply(real)(7, 4)
 	// apply(natural)(1, 2)
-	apply(evenCountingNatural)(7, 1)
+	// apply(evenCountingNatural)(7, 1)
+	// apply(cartesian)([1, 4], [0, 0])
+	apply(lpSpace)([1, 4], [0, 0])
 );
