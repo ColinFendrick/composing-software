b4609510a2318136f8d5bce3395440048ee5d993
Colin Fendrick <colin.fendrick@gmail.com>
2021-02-06 09:04:29 -0800

    refactor metrics

--- metrics.js
+++ metrics.js
@@ -1,21 +1,21 @@
 const apply = ({ metric, range }) => (x1, x2) => range(x1) && range(x2) && metric(x1, x2);
+const metricSpace = (range, metric) => ({ range, metric });
 
-const real = {
-	metric: (x1, x2) => Math.abs(x2 - x1),
-	range: x => typeof x === 'number'
-};
+const realNumbers = x => typeof x === 'number';
+const integers = x => Number.isInteger(x);
+const realMetric = (x1, x2) => Math.abs(x2 - x1);
+const bunchedIntegerMetric = (x1, x2) => x1 % 2 !== 0 || x2 % 2 !== 0 ?
+	(Math.abs(x2 - x1) / 2) + 1 :
+	(Math.abs(x2 - x1) / 2);
 
-const natural = {
-	metric: (x1, x2) => Math.abs(x2, x1),
-	range: x => Number.isInteger(x)
-};
+const real = metricSpace(realNumbers, realMetric);
 
-const evenCountingNatural = {
-	metric: (x1, x2) => x1 % 2 !== 0 || x2 % 2 !== 0
-		? (Math.abs(x2 - x1) / 2) + 1
-		: (Math.abs(x2 - x1) / 2),
-	range: x => Number.isInteger(x)
-};
+const natural = metricSpace(integers, realMetric);
+
+const evenCountingNatural = metricSpace(integers, bunchedIntegerMetric);
+
+// const taxiCabMetric =
+
 
 console.log(
 	// apply(real)(7, 4)
